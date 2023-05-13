187ec74505b4e7c48db6e83f4913097d6220c7ea
AhmedAlyEl-Ghannam <89432827+AhmedAlyEl-Ghannam@users.noreply.github.com>
2023-05-13 22:12:19 +0300

    fixed psdx_db typo

--- Codex_Linea/MultiLevel3.m
+++ Codex_Linea/MultiLevel3.m
@@ -52,7 +52,7 @@ function MultiLevel3(r, bitrate)
     psdx_db = pow2db(psdx);
     
     max_index = find(psdx_db == max(psdx_db));
-    bandwidth_index = find(psdx_db(max_index+1:end) < (psdx(max_index) - 3));
+    bandwidth_index = find(psdx_db(max_index+1:end) < (psdx_db(max_index) - 3));
     bandwidth_MLT3 = freq(bandwidth_index(1) + max_index)
     
     subplot(2,1,2);
