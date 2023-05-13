4062a105d61692f14bbf745649c9080c75e27d81
AhmedAlyEl-Ghannam <89432827+AhmedAlyEl-Ghannam@users.noreply.github.com>
2023-05-13 20:49:43 +0300

    Delete Codex Linea (Line Code). directory

--- Codex Linea (Line Code)./CalculatePSD.m
+++
@@ -1,9 +0,0 @@
-function [psdx, freq] = CalculatePSD(x, dt)
-    fs = 1 / dt;  
-    N = length(x);
-    xdft = fft(x);
-    xdft = xdft(1:N/2+1);
-    psdx = (1/(fs*N)) * abs(xdft).^2;
-    psdx(2:end-1) = 2*psdx(2:end-1);
-    freq = 0:fs/length(x):fs/2;
-end
